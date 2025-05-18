68ae37eb2b57aa02ecd3516b6d60e6882ccc4f7e
sheen2536 <afsheensajad5@gmail.com>
2025-05-18 19:46:13 +0530

    afsheen final Changes

---
+++ NetworkLayer/router.py
@@ -0,0 +1,159 @@
+import ipaddress
+
+class RIPProtocol:
+    def __init__(self, router_id):
+        self.router_id = router_id
+        self.neighbors = {}  # neighbor_id -> cost
+        self.routing_table = {router_id: (0, router_id)}  # destination -> (cost, next_hop)
+
+    def add_neighbor(self, neighbor_id, cost):
+        self.neighbors[neighbor_id] = cost
+        self.routing_table[neighbor_id] = (cost, neighbor_id)
+
+    def receive_vector(self, from_router, vector):
+        updated = False
+        cost_to_neighbor = self.neighbors.get(from_router, float('inf'))
+        for dest, (neighbor_cost, _) in vector.items():
+            new_cost = cost_to_neighbor + neighbor_cost
+            if dest not in self.routing_table or new_cost < self.routing_table[dest][0]:
+                self.routing_table[dest] = (new_cost, from_router)
+                updated = True
+        return updated
+
+    def send_vector(self):
+        return self.routing_table.copy()
+
+    def get_next_hop(self, destination):
+        return self.routing_table.get(destination, (float('inf'), None))[1]
+
+    def print_routing_table(self):
+        print(f"[RIP-{self.router_id}] Routing Table:")
+        for dest, (cost, next_hop) in sorted(self.routing_table.items()):
+            print(f"  Destination: {dest}, Cost: {cost}, Next Hop: {next_hop}")
+        print()
+
+
+def get_network(ip, prefix):
+    return str(ipaddress.ip_network(f"{ip}/{prefix}", strict=False).network_address)
+
+
+class Router:
+    def __init__(self, name, interfaces):
+        self.name = name
+        self.interfaces = interfaces  # iface -> (ip, mac, prefix)
+        self.arp_table = {}
+        self.routing_table = {}  # (network, prefix) -> iface
+        self.interface_links = {}
+        self.pending_packets = {}
+        self.rip = RIPProtocol(name)
+
+        for iface, (ip, mac, prefix) in interfaces.items():
+            network = get_network(ip, prefix)
+            self.routing_table[(network, prefix)] = iface
+
+        print(f"[{self.name}] Routing Table: {self.routing_table}")
+
+    def connect_interface(self, iface, link, peer_iface=None):
+        if peer_iface:
+            self.interface_links[iface] = ('serial', link, peer_iface)
+        else:
+            self.interface_links[iface] = ('switch', link)
+
+    def receive(self, frame, iface):
+        if frame['type'] == 'DATA':
+            self.receive_data(frame)
+        elif frame['type'] == 'ARP_REQUEST':
+            self.receive_arp_request(frame)
+        elif frame['type'] == 'ARP_REPLY':
+            self.receive_arp_reply(frame)
+
+    def receive_arp_request(self, frame):
+        target_ip = frame['target_ip']
+        for iface, (ip, mac, _) in self.interfaces.items():
+            if target_ip == ip:
+                print(f"[{self.name}] received ARP Request for {target_ip} on {iface}, replying directly...")
+                self.arp_table[frame['sender_ip']] = frame['sender_mac']
+                self.print_arp_table()
+                reply = {
+                    'type': 'ARP_REPLY',
+                    'sender_ip': ip,
+                    'sender_mac': mac,
+                    'target_ip': frame['sender_ip'],
+                    'target_mac': frame['sender_mac']
+                }
+                link_type, *link_data = self.interface_links[iface]
+                if link_type == 'switch':
+                    link_data[0].receive_frame(reply, self)
+                elif link_type == 'serial':
+                    link_data[0].transmit(self, reply, iface)
+
+    def receive_arp_reply(self, frame):
+        print(f"[{self.name}] received ARP Reply: {frame['sender_ip']} is at {frame['sender_mac']}")
+        self.arp_table[frame['sender_ip']] = frame['sender_mac']
+        self.print_arp_table()
+        if frame['sender_ip'] in self.pending_packets:
+            pending_frame, out_iface = self.pending_packets.pop(frame['sender_ip'])
+            out_ip, out_mac_self, _ = self.interfaces[out_iface]
+            dst_mac = frame['sender_mac']
+            pending_frame['l2'] = {'src_mac': out_mac_self, 'dst_mac': dst_mac}
+            print(f"[{self.name}] prepares L2 header: src_mac={out_mac_self}, dst_mac={dst_mac}")
+            print(f"[{self.name}] forwarding DATA to {pending_frame['l3']['dst_ip']} via MAC {dst_mac}")
+            link_type, *link_data = self.interface_links[out_iface]
+            if link_type == 'switch':
+                link_data[0].receive_frame(pending_frame, self)
+
+    def receive_data(self, frame):
+        print(f"[{self.name}] received data frame. Stripping L2 and inspecting IP packet...")
+        src_ip = frame['l3']['src_ip']
+        dst_ip = frame['l3']['dst_ip']
+        print(f"[{self.name}] IP Packet: src={src_ip}, dst={dst_ip}")
+
+        best_match = None
+        best_prefix = -1
+        dst_ip_obj = ipaddress.IPv4Address(dst_ip)
+
+        for (net_str, prefix), iface in self.routing_table.items():
+            network = ipaddress.IPv4Network(f"{net_str}/{prefix}", strict=False)
+            if dst_ip_obj in network and prefix > best_prefix:
+                best_match = iface
+                best_prefix = prefix
+
+        if best_match:
+            iface = best_match
+            dst_mac = self.arp_table.get(dst_ip)
+            if dst_mac is None:
+                print(f"[{self.name}] needs MAC of {dst_ip}, sending ARP Request on {iface}")
+                arp_req = {
+                    'type': 'ARP_REQUEST',
+                    'sender_ip': self.interfaces[iface][0],
+                    'sender_mac': self.interfaces[iface][1],
+                    'target_ip': dst_ip
+                }
+                self.pending_packets[dst_ip] = (frame, iface)
+                link_type, *link_data = self.interface_links[iface]
+                if link_type == 'switch':
+                    link_data[0].receive_frame(arp_req, self)
+                elif link_type == 'serial':
+                    link_data[0].transmit(self, arp_req, iface)
+            else:
+                print(f"[{self.name}] MAC for {dst_ip} is {dst_mac}, forwarding data")
+                frame['l2']['src_mac'] = self.interfaces[iface][1]
+                frame['l2']['dst_mac'] = dst_mac
+                link_type, *link_data = self.interface_links[iface]
+                if link_type == 'switch':
+                    link_data[0].receive_frame(frame, self)
+
+    def add_rip_neighbor(self, neighbor_router, cost):
+        self.rip.add_neighbor(neighbor_router.name, cost)
+
+    def exchange_routing_info(self, neighbor_router):
+        updated = neighbor_router.rip.receive_vector(self.name, self.rip.send_vector())
+        return updated
+
+    def print_arp_table(self):
+        print(f"[{self.name}] ARP Table: {self.arp_table}")
+
+    def print_rip_table(self):
+        self.rip.print_routing_table()
+
+
--- NetworkLayer/testcase1.py
+++ NetworkLayer/testcase1.py
@@ -3,6 +3,18 @@ from router import Router
 from switch import Switch
 from serialLink import SerialLink
 
+def run_rip_simulation(routers):
+    changed = True
+    while changed:
+        changed = False
+        for r1 in routers:
+            for r2 in routers:
+                if r1 != r2:
+                    changed |= r1.exchange_routing_info(r2)
+    for router in routers:
+        router.print_rip_table()
+
+# ----- Network Topology -----
 s1 = Switch("Switch1")
 s2 = Switch("Switch2")
 
@@ -12,13 +24,30 @@ r1 = Router("Router1", {
     "eth2": ("22.22.22.1", "AA:BB:CC:DD:EE:02", 24)
 })
 
-# Connect Router interfaces to Switches
-r1.connect_interface("eth1", s1)  # Connect eth1 to Switch1
-r1.connect_interface("eth2", s2)  # Connect eth2 to Switch2
+r2 = Router("Router2", {
+    "eth1": ("33.33.33.1", "AA:BB:CC:DD:EE:03", 24)
+})
 
-# Create Hosts
-h1 = Host("PC-A", "11.11.11.10", "AA:AA:AA:AA:AA:01", "11.11.11.1")  # PC-A, on subnet 11.11.11.0/24
-h2 = Host("PC-B", "22.22.22.40", "AA:AA:AA:AA:AA:02", "22.22.22.1")  # PC-B, on subnet 22.22.22.0/24
+r3 = Router("Router3", {
+    "eth1": ("44.44.44.1", "AA:BB:CC:DD:EE:04", 24)
+})
+
+# Connect Router interfaces to Switches
+r1.connect_interface("eth1", s1)
+r1.connect_interface("eth2", s2)
+
+# RIP Neighbor Setup
+r1.add_rip_neighbor(r2, 1)
+r2.add_rip_neighbor(r1, 1)
+r2.add_rip_neighbor(r3, 2)
+r3.add_rip_neighbor(r2, 2)
+
+# Run RIP Distance Vector Routing Simulation
+run_rip_simulation([r1, r2, r3])
+
+# ----- Hosts -----
+h1 = Host("PC-A", "11.11.11.10", "AA:AA:AA:AA:AA:01", "11.11.11.1")
+h2 = Host("PC-B", "22.22.22.40", "AA:AA:AA:AA:AA:02", "22.22.22.1")
 
 # Connect Hosts to Switches
 h1.connect(s1, 2)  # PC-A on Switch1 port 2
